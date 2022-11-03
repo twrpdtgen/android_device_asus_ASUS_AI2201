0dfafdfb8e5e4d33c8f0080f3e1b9c3a1165a157
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-03 23:54:34 +0100

    ASUS_AI2201: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: asus Model: ASUS_AI2201 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_ASUS_AI2201.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_ASUS_AI2201-user \
+    omni_ASUS_AI2201-userdebug \
+    omni_ASUS_AI2201-eng
